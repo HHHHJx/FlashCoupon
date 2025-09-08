557012baa704664adf131c573d2d255169974b68
AkiiXx <954525281@qq.com>
2025-09-08 16:48:34 +0800

    对优惠券项目的所有模块进行初始化

---
+++ merchant-admin/src/test/java/com/jx/merchantadmin/MerchantAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jx.merchantadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MerchantAdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ search/src/test/java/com/jx/search/SearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jx.search;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SearchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
